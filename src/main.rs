51d6d636a4d9fb377e98ee1b2352df19c32441b1
cyrus01337 <cyrus21337@gmail.com>
2023-04-25 09:34:33 +0100

    Add decryption + global refactor

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,83 @@
 use serde_json;
 use std::env;
 use std::fs;
+use std::process;
 
-fn concatenate_vector_to_string(vector: &Vec<String>) -> String {
-    let mut concatenated = String::new();
+extern crate exitcode;
 
-    for i in 1..vector.len() {
-        let arg = &vector[i];
-        concatenated += arg;
+#[macro_use]
+extern crate lazy_static;
 
-        if i < vector.len() - 1 {
-            concatenated += " ";
-        }
-    }
-
-    concatenated
-}
-
-fn retrieve_morse_code_mapping() -> serde_json::Value {
-    let content = fs::read_to_string("src/morse-code.json").expect("Could not read JSON");
+fn retrieve_morse_code_mapping(name: &str) -> serde_json::Value {
+    let mapping_path = format!("src/{}.json", name);
+    let content = fs::read_to_string(mapping_path).expect("Could not read JSON");
     let morse_code_mapping: serde_json::Value =
         serde_json::from_str(&content).expect("Could not parse JSON");
 
     morse_code_mapping
 }
 
+lazy_static! {
+    static ref SERIALISED_MAPPING: serde_json::Value = retrieve_morse_code_mapping("serialised");
+    static ref DESERIALISED_MAPPING: serde_json::Value =
+        retrieve_morse_code_mapping("deserialised");
+}
+
 fn encrypt_to_morse_code(text: &str) -> String {
     let characters: Vec<char> = text.chars().collect();
-    let morse_code_mapping = retrieve_morse_code_mapping();
-    let mut encrypted = String::new();
+    let mut to_join: Vec<String> = vec![];
 
-    for i in 0..text.len() {
-        let character = characters[i];
-        let morse_code_character = &morse_code_mapping[character.to_string()];
+    for character in characters {
+        let morse_code_character = &SERIALISED_MAPPING[character.to_string()];
 
-        if character == ' ' {
-            encrypted += "🥢⬛";
-
-            continue;
-        } else if morse_code_character.is_null() {
+        if morse_code_character.is_null() {
             continue;
         }
 
-        encrypted += &morse_code_character.to_string().replace("\"", "");
+        let formatted = morse_code_character.to_string().replace("\"", "");
 
-        if i < text.len() - 1 {
-            encrypted += "⬛";
-        }
+        to_join.push(formatted);
     }
 
-    encrypted
+    return to_join.join(" ");
+}
+
+fn decrypt_to_morse_code(text: &str) -> String {
+    let characters: Vec<&str> = text.split(" ").collect();
+    let mut decrypted = String::new();
+
+    for character in characters.iter() {
+        let morse_code_character = &DESERIALISED_MAPPING[character.to_string()];
+
+        if morse_code_character.is_null() {
+            continue;
+        }
+
+        let formatted = &morse_code_character.to_string().replace("\"", "");
+
+        decrypted += formatted;
+    }
+
+    decrypted
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let text = concatenate_vector_to_string(&args);
-    let encrypted = encrypt_to_morse_code(&text);
+    let mut result = String::new();
+    let mut args = env::args();
+    let subcommand = args.nth(1).unwrap().to_ascii_lowercase();
+    let to_concatenate: Vec<String> = args.collect();
+    let text = to_concatenate.join(" ");
 
-    println!("{}", encrypted);
+    if text == "" {
+        process::exit(exitcode::DATAERR);
+    }
+
+    if subcommand == "encrypt" {
+        result = encrypt_to_morse_code(&text);
+    } else if subcommand == "decrypt" {
+        result = decrypt_to_morse_code(&text);
+    }
+
+    println!("{}", result);
+    process::exit(exitcode::OK);
 }
